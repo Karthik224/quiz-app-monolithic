443c6965f12fb372d8dcd87fc9492698dbab3327
Karthik Paila <karthikpaila224@gmail.com>
2026-01-05 23:20:03 +0530

    adding quiz/get/1 api to get the created questions from db

--- src/main/java/com/kartp/quizapp/controller/QuizController.java
+++ src/main/java/com/kartp/quizapp/controller/QuizController.java
@@ -1,13 +1,14 @@
 package com.kartp.quizapp.controller;
 
+import com.kartp.quizapp.model.Question;
+import com.kartp.quizapp.model.QuestionWrapper;
 import com.kartp.quizapp.services.QuizService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("quiz")
@@ -22,6 +23,10 @@ public class QuizController {
 //        return new ResponseEntity<>("all good", HttpStatus.OK);
 
         return quizService.createQuiz(category,numQue,title);
+    }
 
+    @GetMapping("get/{id}")
+    public ResponseEntity<List<QuestionWrapper>> getQuizQuestions(@PathVariable Integer id){
+        return quizService.getQuestions(id);
     }
 }
--- src/main/java/com/kartp/quizapp/services/QuizService.java
+++ src/main/java/com/kartp/quizapp/services/QuizService.java
@@ -2,13 +2,17 @@ package com.kartp.quizapp.services;
 import com.kartp.quizapp.dao.QuestionDao;
 import com.kartp.quizapp.dao.QuizDao;
 import com.kartp.quizapp.model.Question;
+import com.kartp.quizapp.model.QuestionWrapper;
 import com.kartp.quizapp.model.Quiz;
 import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class QuizService {
@@ -30,5 +34,17 @@ public class QuizService {
         quizDao.save(quiz);
 
         return new ResponseEntity<>("SUCCESS table created", HttpStatus.CREATED);
+    }
+
+    public ResponseEntity<List<QuestionWrapper>> getQuestions(Integer id) {
+        Optional<Quiz> quiz= quizDao.findById(id);
+        List<Question> questionFromDB = quiz.get().getQuestions();
+        List<QuestionWrapper> questionsForUser = new ArrayList<>();
+        for(Question q : questionFromDB){
+            QuestionWrapper qw= new QuestionWrapper(q.getId(),q.getQuestionTitle(),q.getOption1(),q.getOption2(),q.getOption3(),q.getOption4());
+            questionsForUser.add(qw);
+        }
+
+        return new ResponseEntity<>(questionsForUser,HttpStatus.OK);
     }
 }
